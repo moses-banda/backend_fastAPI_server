17feaf8177dbdbd62a14053d8784b4c910cb83de
Banda <moses.banda@vanderbilt.edu>
2026-02-09 18:21:31 -0600

    Update main.py for deployment

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from db import superbase
 from schemas import WaitlistRequest
+from postgrest.exceptions import APIError
 
 app = FastAPI()
 
@@ -15,14 +16,20 @@ app.add_middleware(
 @app.post("/waitlist")
 def join_waitlist(data: WaitlistRequest):
     email = data.email.lower()
-    response = superbase.table("waitlist").insert({
-        "email" : email
-    }).execute()
-
-    if response.error:
-        if "duplicate key" in response.error.message.lower():
+    try:
+        response = superbase.table("waitlist").insert({
+            "email" : email
+        }).execute()
+        
+    except APIError as e:
+        # Check for duplicate key error (Postgres code 23505)
+        if e.code == "23505":
             raise HTTPException(status_code=409, detail="already on the waitlist")
+        print(f"Database Error: {e}")
         raise HTTPException(status_code=500, detail="Database Error")
+    except Exception as e:
+        print(f"Unexpected Error: {e}")
+        raise HTTPException(status_code=500, detail="Internal Server Error")
     
     return{
         "success": True,
